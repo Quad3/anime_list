a6059e86597515f0e0a6c623d7214a24db8df11e
Quad3 <bbulat1990@gmail.com>
2024-08-01 14:02:31 +0900

    Add anime model

---
+++ src/models.py
@@ -0,0 +1,23 @@
+import uuid
+import enum
+from sqlalchemy import Column, JSON, String, UUID, Integer
+from sqlalchemy.types import Enum
+
+from database import Base
+
+
+class State(str, enum.Enum):
+    WATCHING = "WATCHING"
+    WATCHED = "WATCHED"
+    DROPPED = "DROPPED"
+
+
+class Anime(Base):
+    __tablename__ = "anime"
+
+    uuid = Column(UUID, primary_key=True, default=uuid.uuid4)
+    name = Column(String(128), nullable=False)
+    state = Column(Enum(State), default="WATCHING")
+    rate = Column(Integer, nullable=True)
+    review = Column(String(4096), nullable=True)
+    from_to = Column(JSON, nullable=False)
