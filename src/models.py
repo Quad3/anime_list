d8be95c612c7f05d9bfa5740397a48b7ae633430
Quad3 <bbulat1990@gmail.com>
2024-08-01 14:52:48 +0900

    Add unique constraint to Anime name

--- src/models.py
+++ src/models.py
@@ -16,7 +16,7 @@ class Anime(Base):
     __tablename__ = "anime"
 
     uuid = Column(UUID, primary_key=True, default=uuid.uuid4)
-    name = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False, unique=True)
     state = Column(Enum(State), default="WATCHING")
     rate = Column(Integer, nullable=True)
     review = Column(String(4096), nullable=True)
