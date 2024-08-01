65468ac36d28f5a239eb1f60a9d8a5e3bf836821
Quad3 <bbulat1990@gmail.com>
2024-08-01 11:34:47 +0900

    Add src/database.py

---
+++ src/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+Base = declarative_base()
+
+engine = create_engine(DATABASE_URL)
+session = sessionmaker(bind=engine)
+
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
