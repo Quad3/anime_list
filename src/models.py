b800a0697f26dc3ecfe4301a76a6394a26701763
Quad3 <bbulat1990@gmail.com>
2024-09-04 16:17:10 +0900

    Change anime from_to field to foreign key

--- src/models.py
+++ src/models.py
@@ -1,7 +1,8 @@
 import uuid
 import enum
-from sqlalchemy import Column, JSON, String, UUID, Integer
+from sqlalchemy import Column, String, UUID, Integer, Date, ForeignKey
 from sqlalchemy.types import Enum
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -20,4 +21,16 @@ class Anime(Base):
     state = Column(Enum(State), default="WATCHING")
     rate = Column(Integer, nullable=True)
     review = Column(String(4096), nullable=True)
-    from_to = Column(JSON, nullable=False)
+
+    from_to = relationship("AnimeFromTo", back_populates="anime_id")
+
+
+class AnimeFromTo(Base):
+    __tablename__ = "anime_from_to"
+
+    id = Column(Integer, primary_key=True)
+    from_ = Column(Date, name="from")
+    to = Column(Date, nullable=True)
+    anime_id = Column(UUID, ForeignKey("anime.uuid"))
+
+    anime = relationship("Anime", back_populates="from_to")
