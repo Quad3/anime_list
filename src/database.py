8271e593cc4d051077b695daf116a2f09de19185
Quad3 <bbulat1990@gmail.com>
2024-09-24 17:31:49 +0900

    Import fix

--- src/database.py
+++ src/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
